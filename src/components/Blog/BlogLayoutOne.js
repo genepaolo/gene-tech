d72ec8c49fbff7f24a335aa2c1546b641c74550b
Gene Flores <goflores@usc.edu>
2024-03-15 12:49:07 -0700

    Tweaks

--- src/components/Blog/BlogLayoutOne.js
+++ src/components/Blog/BlogLayoutOne.js
@@ -19,7 +19,7 @@ const BlogLayoutOne = ({ blog }) => {
         alt={blog.title}
         width={blog.image.width}
         height={blog.image.height}
-        className="w-full h-full object-center object-cover rounded-xl group-hover:scale-105 transition-all ease duration-300"
+        className="h-full object-center object-cover rounded-xl group-hover:scale-105 transition-all ease duration-300"
         sizes="(max-width: 1180px) 100vw, 50vw"
       />
 
--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -6,7 +6,7 @@ const siteMetadata = {
     description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
     language: 'en-us',
     theme: 'system', // system, dark or light
-    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteUrl: 'https://gene-tech.vercel.app/', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social-banner.png', // add social banner in the public folder
     email: 'paolo.gene@gmail.com', 
